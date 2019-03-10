5502529ef8d129986dd282eb7ace2805b8ccb2b5
Aaron Clauson <aaron@sipsorcery.com>
2019-03-10 22:27:49 +0100

    Tidy up and refactor of msbuild project files, scripts and includes (should mean ready for vs2019). Add options to set tool paths in msvc python config script.

--- src/compat/byteswap.h
+++ src/compat/byteswap.h
@@ -10,6 +10,7 @@
 #endif
 
 #include <stdint.h>
+#include <stdlib.h>
 
 #if defined(HAVE_BYTESWAP_H)
 #include <byteswap.h>
@@ -29,6 +30,20 @@
 
 #endif // !defined(bswap_16)
 
+#elif _MSC_VER
+
+#if HAVE_DECL_BSWAP_16 == 0
+#define bswap_16(x) _byteswap_ushort(x)
+#endif
+
+#if HAVE_DECL_BSWAP_32 == 0
+#define bswap_32(x) _byteswap_ulong(x)
+#endif
+
+#if HAVE_DECL_BSWAP_64 == 0
+#define bswap_64(x) _byteswap_uint64(x)
+#endif
+
 #else
 // Non-Mac OS X / non-Darwin
 
@@ -42,22 +57,15 @@ inline uint16_t bswap_16(uint16_t x)
 #if HAVE_DECL_BSWAP_32 == 0
 inline uint32_t bswap_32(uint32_t x)
 {
-    return (((x & 0xff000000U) >> 24) | ((x & 0x00ff0000U) >>  8) |
-            ((x & 0x0000ff00U) <<  8) | ((x & 0x000000ffU) << 24));
+    return (((x & 0xff000000U) >> 24) | ((x & 0x00ff0000U) >> 8) |
+            ((x & 0x0000ff00U) << 8) | ((x & 0x000000ffU) << 24));
 }
 #endif // HAVE_DECL_BSWAP32 == 0
 
 #if HAVE_DECL_BSWAP_64 == 0
 inline uint64_t bswap_64(uint64_t x)
 {
-     return (((x & 0xff00000000000000ull) >> 56)
-          | ((x & 0x00ff000000000000ull) >> 40)
-          | ((x & 0x0000ff0000000000ull) >> 24)
-          | ((x & 0x000000ff00000000ull) >> 8)
-          | ((x & 0x00000000ff000000ull) << 8)
-          | ((x & 0x0000000000ff0000ull) << 24)
-          | ((x & 0x000000000000ff00ull) << 40)
-          | ((x & 0x00000000000000ffull) << 56));
+    return (((x & 0xff00000000000000ull) >> 56) | ((x & 0x00ff000000000000ull) >> 40) | ((x & 0x0000ff0000000000ull) >> 24) | ((x & 0x000000ff00000000ull) >> 8) | ((x & 0x00000000ff000000ull) << 8) | ((x & 0x0000000000ff0000ull) << 24) | ((x & 0x000000000000ff00ull) << 40) | ((x & 0x00000000000000ffull) << 56));
 }
 #endif // HAVE_DECL_BSWAP64 == 0
 
